652def5972dba5b0d7a1afc7e3e5570dc6291955
ycm76229 <ycm76229@gmail.com>
2023-08-10 13:39:59 +0800

    update baseUrl

--- src/config.tsx
+++ src/config.tsx
@@ -1,5 +1,5 @@
 export default {
-  baseUrl: "https://me.hjkl01.cn:19001",
+  // baseUrl: "https://me.hjkl01.cn:19001",
   // baseUri : "http://localhost:7109",
-  // baseUri : "http://dj.cpgroup.unclepi.cool",
+  baseUrl: "http://dj.cpgroup.unclepi.cool",
 };
